5c506f171936b608714dacacda1219b05d200690
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-09-06 06:31:52 +0200

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { useRef, useState } from 'react';
+
 const App = () => {
-  return <h1>App</h1>;
+  const [text, setText] = useState('');
+  const [list, setList] = useState([]);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      return;
+    }
+    setList((prevList) => [...prevList, text]);
+    setText('');
+  };
+
+  return (
+    <>
+      <h2>React Project Ideas</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={text}
+          placeholder="Idea"
+        />
+        <button>Submit</button>
+      </form>
+
+      <ol>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ol>
+    </>
+  );
 };
 
 export default App;
