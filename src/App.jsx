7fd524ebc95089b55f5b8f9d2217d9715a0afbd2
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-09-22 04:59:12 +0200

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import Product from './Product.jsx';
 import productsData from './data.jsx';
-import { slowCountItems } from './utils.jsx';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [sort, setSort] = useState(false);
 
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
@@ -14,7 +14,33 @@ const App = () => {
     setCount((prevCount) => prevCount - 1);
   };
 
-  const productsCount = slowCountItems(productsData, 500);
+  // const startTime1 = Date.now();
+
+  // const sortedProducts = [...productsData].sort((a, b) =>
+  //   a.name.localeCompare(b.name)
+  // );
+
+  // const endTime1 = Date.now();
+  // console.log(`Took ${endTime1 - startTime1}ms `);
+
+  const startTime2 = Date.now();
+
+  /**
+   * Challenge: memoize the sorting calculation of sortedProducts
+   * so that it only happens if the value of "sort" changes.
+   *
+   * Make sure to comment out the version
+   * above when testing your version here
+   */
+
+  const sortedProducts = useMemo(() => {
+    return [...productsData].sort((a, b) => a.name.localeCompare(b.name));
+  }, [productsData]);
+
+  const endTime2 = Date.now();
+  console.log(`Took ${endTime2 - startTime2}ms`);
+
+  const visibleProducts = sort ? sortedProducts : productsData;
 
   return (
     <>
@@ -27,9 +53,13 @@ const App = () => {
       </button>{' '}
       <br />
       <br />
-      <h2>There are {productsCount} products </h2>
+      <button onClick={() => setSort((prev) => !prev)} className="button">
+        {sort ? 'Unsort' : 'Sort'}
+      </button>
+      <br />
+      <br />
       <div className="products-list">
-        {productsData.map((product) => (
+        {visibleProducts.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </div>
